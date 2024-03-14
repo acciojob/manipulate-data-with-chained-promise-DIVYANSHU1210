c02e308f267cd71979c2207b501bba05dedd1379
DIVYANSHU1210 <84267089+DIVYANSHU1210@users.noreply.github.com>
2024-03-14 16:22:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+//your JS code here. If required
+function myPromise (numbers) {
+	new Promise((resolve, reject)=>{
+	setTimeOut(()=>{
+		const filteredArray = numbers.filter((num) => num%2 === 0);
+		resolve(filteredArray)
+	}, 2000)).then((filteredArray) => {
+		new Promise((resolve, reject)=>{
+			const resultArray = numbers.map((num)=> num*2);
+			resolve(resultArray)
+		}, 1000)
+	})
+} 
+
+const inputArray = [1, 2, 3, 4];
+
+const outputDiv = document.getElementById("output");
+
+const myPromise(inputArray)
+	.then(result=>outputDiv.innerText = `${result.join(', ')}`)
+	.catch(err => console.log(err))
+
+
+
