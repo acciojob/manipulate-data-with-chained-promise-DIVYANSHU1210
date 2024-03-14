19b087a40813cc6eabf4a6caa086357403f5e42d
DIVYANSHU1210 <84267089+DIVYANSHU1210@users.noreply.github.com>
2024-03-14 16:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 //your JS code here. If required
 function myPromise (numbers) {
-	new Promise((resolve, reject)=>{
-	setTimeOut(()=>{
-		const filteredArray = numbers.filter((num) => num%2 === 0);
-		resolve(filteredArray)
-	}, 2000)).then((filteredArray) => {
-		new Promise((resolve, reject)=>{
-			const resultArray = numbers.map((num)=> num*2);
-			resolve(resultArray)
-		}, 1000)
-	})
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			const filteredArray = numbers.filter((num) => num%2 === 0);
+			resolve(filteredArray)
+		}, 1000);
+	}).then((filteredArray) => {
+		return new Promise((resolve, reject)=>{
+			setTimeout(()=>{
+				const resultArray = filteredArray.map((num)=> num*2);
+				resolve(resultArray)
+			}, 2000)
+		});
+	});
 } 
 
 const inputArray = [1, 2, 3, 4];
 
 const outputDiv = document.getElementById("output");
+outputDiv.innerText = `initial Array-> [${inputArray.join(', ')}]`;
 
-const myPromise(inputArray)
-	.then(result=>outputDiv.innerText = `${result.join(', ')}`)
+myPromise(inputArray)
+	.then(result=>outputDiv.innerText += `\nfiltered arrays - [${result.join(', ')}]`)
 	.catch(err => console.log(err))
 
 
